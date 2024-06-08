50b96b0ad56671be9b0bfeb634608f41b119f2fb
ChristianIshimwe7 <c.ishimwe7@alustudent.com>
2024-06-08 04:44:37 -0700

    correction

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-const Square5 = require('./5-square');
-
-class Square extends Square5 {
-  charPrint(c) {
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
-}
-
-module.exports = Square;
-
+};
