107e37a4ad5670594de4302977c2685fec596097
ChristianIshimwe7 <c.ishimwe7@alustudent.com>
2024-06-08 02:07:06 -0700

    6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square') {
-  charPrint(c) {
-    const char = (typeof c === 'string' && c.length === 1) ? c : 'X';
-    for (let i = 0; i < this.height; i++) {
-      console.log(char.repeat(this.width));
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 };
